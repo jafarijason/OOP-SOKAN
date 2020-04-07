d944f9838b37ab12fb10e996a6f9a02fe2401d32
JasonJafariHome <jason.jafari.m@gmail.com>
2020-04-07 00:40:53 -0400

    1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+class User
+{
+    public $name = "Jason";
+    public $lastName = "Jafari";
+    public $dob = 1985;
+
+    public function showFullName()
+    {
+        return this->name.' '.this->lastName;
+    }
+}
+
+
+?>
